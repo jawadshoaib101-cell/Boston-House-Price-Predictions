f87e628a2e1db586ccdd342aa42aaa88be374436
Jawad <jawad.shoaib.101@gmail.com>
2025-12-15 19:22:56 -0500

    Lasso and Ridge Regression. Comparing MSE and MAE between linear reg, lasso reg, and ridge reg.

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 # Splitting data
 df = pd.read_csv("Boston (1).csv").sample(frac=1)
@@ -26,13 +27,37 @@ test, test_x, test_y = scaling(test)
 # Using the Linear Regression
 linear_regression = LinearRegression()
 linear_regression.fit(train_x, train_y)
-y_pred = linear_regression.predict(test_x)
-comparison = pd.DataFrame({
+y_pred_linear_reg = linear_regression.predict(test_x)
+comparison_linear_regression = pd.DataFrame({
     "Actual": test_y,
-    "Prediction": y_pred
+    "Prediction": y_pred_linear_reg
 })
-print(comparison.head(30))
-plt.scatter(test_y, y_pred)
+
+# Using Ridge Regression (L2)
+ridge_regression = Ridge()
+ridge_regression.fit(train_x, train_y)
+y_pred_ridge = ridge_regression.predict(test_x)
+comparison_ridge = pd.DataFrame({
+    "Actual": test_y,
+    "Prediction": y_pred_ridge
+})
+
+# Using Lasso Regression (L1)
+lasso_regression = Lasso()
+lasso_regression.fit(train_x, train_y)
+y_pred_lasso = lasso_regression.predict(test_x)
+# MSE and MAE
+ridge_mse = mean_squared_error(test_y, y_pred_ridge)
+linear_reg_mse = mean_squared_error(test_y, y_pred_linear_reg)
+ridge_mae = mean_absolute_error(test_y, y_pred_ridge)
+linear_reg_mae = mean_absolute_error(test_y, y_pred_linear_reg)
+lasso_reg_mse = mean_squared_error(test_y, y_pred_lasso)
+lasso_reg_mae = mean_absolute_error(test_y, y_pred_lasso)
+print(f"The MSE for ridge regression: {ridge_mse}. \nThe MSE for linear regression: {linear_reg_mse}."
+      f"\nThe MSE for ridge regression: {ridge_mae}. \nThe MAE for linear regression: {linear_reg_mae}"
+      f"\nThe MSE for lasso regression: {lasso_reg_mse}. \nThe MAE for linear regression: {lasso_reg_mae}")
+
+plt.scatter(test_y, y_pred_linear_reg)
 plt.xlabel('Actual MEDV')
 plt.ylabel('Predicted MEDV')
 plt.title('Actual vs Predicted MEDV')
